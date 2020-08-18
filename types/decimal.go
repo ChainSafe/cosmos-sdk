c3625527296773e2a39863797f0e57c69f0cd054
Federico Kunze <federico.kunze94@gmail.com>
2020-08-18 16:06:08 +0200

    fix panic

--- types/decimal.go
+++ types/decimal.go
@@ -209,6 +209,9 @@ func (d Dec) Abs() Dec          { return Dec{new(big.Int).Abs(d.i)} } // absolut
 
 // BigInt returns a copy of the underlying big.Int.
 func (d Dec) BigInt() *big.Int {
+	if d.IsNil() {
+		return nil
+	}
 	copy := new(big.Int)
 	return copy.Set(d.i)
 }
